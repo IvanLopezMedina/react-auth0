9eb5cd966d695dd4f86bc7f850be702ee33a841b
Ivan <ivan.lopez.medina.ilm@gmail.com>
2020-04-21 22:19:29 +0200

    Implemented login

--- src/App.js
+++ src/App.js
@@ -3,17 +3,28 @@ import {Route} from 'react-router-dom';
 import Home from './Home';
 import Profile from './Profile';
 import Nav from './Nav';
+import Auth from './Auth/Auth';
+import Callback from './Callback';
 
-function App() {
+const App = (props) => {
+  const auth = new Auth(props.history);
   return (
     <>
       <Nav />
       <div className="body">
-        <Route path="/" exact component={Home} />
+        <Route
+          path="/"
+          exact
+          render={(props) => <Home auth={auth} {...props} />}
+        />
+        <Route
+          path="/callback"
+          render={(props) => <Callback auth={auth} {...props} />}
+        />
         <Route path="/profile" component={Profile} />
       </div>
     </>
   );
-}
+};
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-const Home = () => {
+const Home = ({auth}) => {
   return (
     <div>
       <h1>Home</h1>
+      {auth.isAuthenticated() ? (
+        <Link to="/profile">View Profile</Link>
+      ) : (
+        <button onClick={auth.login}>Log In</button>
+      )}
     </div>
   );
 };
